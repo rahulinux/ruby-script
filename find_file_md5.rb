7bd4101011e0a4f81599f484bbe24ee02c5036cf
Rahul Patil <loginrahul90@gmail.com>
2014-03-11 17:08:51 +0530

    Create find_file_md5.rb

---
+++ find_file_md5.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby -w
+require 'find'
+require 'digest/md5'
+
+# Find files based on it's md5sum 
+# http://unix.stackexchange.com/questions/119098/is-there-any-way-to-find-a-file-when-you-know-its-checksum/119111?noredirect=1#119111
+
+file_md5sum_to_match = [ '304a5fa2727ff9e6e101696a16cb0fc5',
+                         '0ce6742445e7f4eae3d32b35159af982' ]
+
+Find.find('/') do |f|
+  next if /(^\.|^\/proc|^\/sys)/.match(f) # skip
+  next unless File.file?(f)
+  begin
+        md5sum = Digest::MD5.hexdigest(File.read(f))
+  rescue
+        puts "Error reading #{f} --- MD5 hash not computed."
+  end
+  if file_md5sum_to_match.include?(md5sum)
+       puts "File Found at: #{f}"
+       file_md5sum_to_match.delete(md5sum)
+  end
+  file_md5sum_to_match.empty? && exit # if array empty then exit
+
+end
