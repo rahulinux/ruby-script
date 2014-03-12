a8e40cf2690d84ed2761c21fb450e5efe10b45c3
Rahul Patil <loginrahul90@gmail.com>
2014-03-12 12:23:40 +0530

    Create fix_filename.rb

---
+++ fix_filename.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/ruby
+
+unless ARGV[0]
+  puts "Usage: ruby fixFilename.rb <filename.ext>"
+  puts "Example: ruby fixFilename.rb \'How to (make) 20% more on $500.pdf\'"
+  exit
+end
+
+
+old_filename = ARGV[0]
+unless File.exist?(old_filename)
+  puts "#{old_filename} does not exist. Please try again."
+  exit
+end
+
+name = File.basename(old_filename, ".*")
+ext = File.extname(old_filename)
+replacements = { /;/ => "-",
+				/\s/ => "_",
+				/\'\`/ => "=",
+				/\&/ => "_and_",
+				/\$/ => "dollar_",
+				/%/ => "_percent",
+				/[\(\)\[\]<>]/ => ""
+				}
+
+replacements.each do |orig, fix|
+  name.gsub!(orig,fix)
+end
+
+File.rename(old_filename, name + ext)
+puts "#{old_filename} ---> #{name + ext}"
